f0c9d22e2126d3f95a00a492114e18e9897b2c71
SuryaAndroid <magnetcreativespub@gmail.com>
2022-03-21 14:34:08 +0530

    Surya Commits 02:31 PM 21/03

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,9 @@ void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,3 +16,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
